b3a5a501ae8b8b017571d8f97b9ec0a1c274da0b
Itay <itaynewman@gmial.com>
2024-06-10 22:04:04 +0300

    new

--- Week8/Alien.py
+++ Week8/Alien.py
@@ -1,10 +1,3 @@
-from scapy.all import *
-
-ALIEN_IP = '54.71.128.194'
-
-
-def filter_func(packet):
-    return IP in packet and packet[IP].src == ALIEN_IP
 
 
 def custom_cipher_decipher(string, caesar_key):
@@ -14,32 +7,13 @@ def custom_cipher_decipher(string, caesar_key):
         if i % 2 != 0 or not char.isalpha():
             result.append(char)
         else:
-            new_char = chr((ord(char) - ord('a') - error_number) % 26 + ord('a')) if char.islower() else chr(
-                (ord(char) - ord('A') - error_number) % 26 + ord('A'))
+            new_char = chr((ord(char) - ord('a') - error_number) % 26 + ord('a')) if char.islower() else chr((ord(char) - ord('A') - error_number) % 26 + ord('A'))
             result.append(new_char)
     return ''.join(result)
 
 
-def extract_caesar_key(payload):
-    for i in range(len(payload) - 6):
-        if payload[i + 3:i + 6].isdigit():
-            return payload[i:i + 6]
-    return None
-
-
-def handle_packet(packet):
-    if filter_func(packet):
-        payload = packet[Raw].load.decode()
-        caesar_key = extract_caesar_key(payload)
-        if caesar_key:
-            deciphered_payload = custom_cipher_decipher(payload[len(caesar_key):], caesar_key)
-            print("Deciphered payload:", deciphered_payload)
-        else:
-            print("Caesar key not found in the payload.")
-
-
 def main():
-    sniff(prn=handle_packet)
+    print(custom_cipher_decipher("zrrnj sldnht! wo iqiwidth d fope bdcn, poedsh hnweu whh qape oi rnh rf tke poaqews iq whh vooau vyvthm.", "ERR003"))
 
 
 if __name__ == '__main__':
