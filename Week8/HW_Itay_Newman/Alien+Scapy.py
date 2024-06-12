333e27603b64a882183540d353617a4922ac97c4
Itay <itaynewman@gmial.com>
2024-06-12 20:59:55 +0300

    new

--- Week8/HW_Itay_Newman/Alien+Scapy.py
+++ Week8/HW_Itay_Newman/Alien+Scapy.py
@@ -1,5 +1,6 @@
 from scapy.all import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, UDP
+import hashlib
 
 ALIEN_IP = '54.71.128.194'
 
@@ -15,8 +16,7 @@ def custom_cipher_decipher(string, caesar_key):
         if i % 2 != 0 or not char.isalpha():
             result.append(char)
         else:
-            new_char = chr((ord(char) - ord('a') - error_number) % 26 + ord('a')) if char.islower() else chr(
-                (ord(char) - ord('A') - error_number) % 26 + ord('A'))
+            new_char = chr((ord(char) - ord('a') - error_number) % 26 + ord('a')) if char.islower() else chr((ord(char) - ord('A') - error_number) % 26 + ord('A'))
             result.append(new_char)
     return ''.join(result)
 
@@ -29,17 +29,29 @@ def extract_caesar_key(payload):
 
 
 def handle_packet(packet):
+    global last_ten_chars_list
     if filter_func(packet):
         payload = packet[Raw].load.decode()
         caesar_key = extract_caesar_key(payload)
         if caesar_key:
             deciphered_payload = custom_cipher_decipher(payload[len(caesar_key):], caesar_key)
             print("Deciphered payload:", deciphered_payload)
+            if "location data" in deciphered_payload:
+                last_ten_chars = deciphered_payload[-10:]
+                last_ten_chars_list.append(last_ten_chars)
+            elif "timed out" in deciphered_payload:
+                all_last_ten_chars = ''.join(last_ten_chars_list)
+                all_last_ten_chars = all_last_ten_chars[:100]
+                md5_hash = hashlib.md5(all_last_ten_chars.encode()).hexdigest()
+                # Send back the MD5 hash string
+                send(IP(dst=packet[IP].src) / UDP() / md5_hash)
         else:
             print("Caesar key not found in the payload.")
 
 
 def main():
+    global last_ten_chars_list
+    last_ten_chars_list = []
     sniff(prn=handle_packet)
 
 
